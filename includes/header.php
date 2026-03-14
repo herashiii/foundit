d8b4511f9634d9ff6c231af8d204ae51be5cfb8e
dandyrrom <dannicromo@gmail.com>
2026-03-14 17:39:19 +0800

    title

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,7 @@ $current_page = basename($_SERVER['PHP_SELF'], ".php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FoundiT - Campus Lost & Found</title>
+    <title>FoundiT - Silliman University Lost & Found</title>
 
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
